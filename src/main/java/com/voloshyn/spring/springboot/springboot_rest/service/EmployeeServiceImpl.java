2f534cb543804aa12073ae48b65a69f7d1aa66d1
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2023-05-25 19:57:58 +0300

    create in MyRestController method showAllEmployees

---
+++ src/main/java/com/voloshyn/spring/springboot/springboot_rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.voloshyn.spring.springboot.springboot_rest.service;
+
+
+import com.voloshyn.spring.springboot.springboot_rest.dao.EmployeeDAO;
+import com.voloshyn.spring.springboot.springboot_rest.entity.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+;
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    @Autowired
+    private EmployeeDAO employeeDAO;
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        List<Employee> allEmployee = employeeDAO.getAllEmployees();
+        return allEmployee;
+    }
+
+//    @Override
+//    @Transactional
+//    public void saveEmployee(Employee employee) {
+//        employeeDAO.saveEmployee(employee);
+//
+//    }
+//
+//    @Override
+//    @Transactional
+//    public Employee getEmployee(int id) {
+//        return employeeDAO.getEmployee(id);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void deleteEmployee(int id) {
+//        employeeDAO.deleteEmployee(id);
+//    }
+}
